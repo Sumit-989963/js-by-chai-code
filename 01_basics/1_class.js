0ebaffc0ce0588213bf7ea41a95a3c7b27a4e56b
Sumit-989963 <sumit83770@gmail.com>
2025-03-23 10:57:39 +0000

    continue config

---
+++ 01_basics/1_class.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "sumit@google.com"
+var accountPassword ="9899639352"
+accountcity = "Delhi"
+
+// const is uses for constant value and it can't be change
+// let is uses for variable value
+// var is uses for global value
+
+accountEmail = "sr@sr.com"
+accountPassword = "1234567890"
+accountcity = "Mumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountcity])
+
