d11423154f9a3cbcf2938f3a8cb50d7cc8674dd2
Sumit-989963 <sumit83770@gmail.com>
2025-03-27 11:52:47 +0000

    Data Conversion

--- 01_basics/02_typeconversion.js
+++ 01_basics/02_typeconversion.js
@@ -7,3 +7,11 @@ let valueNumber = Number(score)
 console.log(valueNumber)
 console.log(typeof valueNumber)
 
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+// "" => 0
+// "" => false
+// "Sumit" => true
