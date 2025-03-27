a2745b20f29eb79c07a91d37cc57d515bfae0b0d
Sumit-989963 <sumit83770@gmail.com>
2025-03-27 11:42:01 +0000

    Data Cnversion

---
+++ 01_basics/02_typeconversion.js
@@ -0,0 +1,9 @@
+let score = "33"
+console.log(score)
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueNumber = Number(score)
+console.log(valueNumber)
+console.log(typeof valueNumber)
+
